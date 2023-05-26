18860a4df320153c6e6362327e0ec28d1c1fac91
Hugo Haas <hugoh@hugoh.net>
2023-05-26 18:25:36 -0500

    Docker image latest (#3)
    * Fixed tmo_monitor.gateway module installation
    
    * Update README.md
    
    add Windows directions
    
    * use meaningful exit status codes, fixes #52
    
    * clarify that a clean run will exit with status code 0 as expected
    
    * add project logo
    
    * First commit to log to syslog - not working yet
    
    Moved all the logging from print_and_log to logging, but syslog doesn't
    work. Need to debug.
    
    * Made Linux syslog logging work
    
    * Moved away from f-Strings to lower Python version requirement
    
    * Added support for Windows (untested)
    
    * Fixed log message format
    
    * Fixed out-of-scope use of syslog_socket
    
    * Fixed missing leading space
    
    * add missing import
    
    * fixes #56 breaking change to webapp login after Nokia firmware upgrade to 1.2103.00.0338
    
    * version bump
    
    * add IPv6 ping support, fixes #43
    
    * use ping6 binary if available for ipv6 ping functionality, fixes #43
    
    * fix setting IPv6 ping via .env
    
    * conditionally check for lsid cookie to avoid runtime error with nokia 2104 firmware update
    
    * calculate arcaydyan eNB ID using decimal base fixes #68 caused by firmware update
    
    * safely access 5g band info for arcadyan gateway - fixes #75
    
    * add support for alternative connectivity checks - fixes #13 #78 ; add explicit support for Sagecom gateway
    
    * correct README
    
    * explicitly pass -4 flag to ping command to try ipv4 with ping command except for OS X clients, fixes #77
    
    ---------
    
    Co-authored-by: highvolt-dev <highvoltage@gmail.com>

--- tmo_monitor/gateway/arcadyan.py
+++ tmo_monitor/gateway/arcadyan.py
@@ -3,6 +3,7 @@ import sys
 import logging
 import math
 from .base import ControllerBase
+from ..status import ExitStatus
 
 class CubeController(ControllerBase):
   def __init__(self, username, password):
@@ -16,7 +17,7 @@ class CubeController(ControllerBase):
       login_request = requests.post('http://192.168.12.1/TMI/v1/auth/login', json={'username': self.username, 'password': self.password})
     except:
       logging.critical("Could not post login request, exiting.")
-      sys.exit(2)
+      sys.exit(ExitStatus.API_ERROR.value)
     login_request.raise_for_status()
     self.app_token = login_request.json()['auth']['token']
 
@@ -27,13 +28,13 @@ class CubeController(ControllerBase):
       stat_request = requests.get('http://192.168.12.1/TMI/v1/network/telemetry?get=all', headers={'Authorization': 'Bearer ' + self.app_token})
     except:
       logging.critical("Could not query site info, exiting.")
-      sys.exit(2)
+      sys.exit(ExitStatus.API_ERROR.value)
 
     stat_request.raise_for_status()
     meta = stat_request.json()['cell']['4g']
 
     return {
-      'eNBID': math.floor(int(meta['ecgi'][6:], 16)/256),
+      'eNBID': math.floor(int(meta['ecgi'][6:])/256),
       'PLMN': meta['mcc'] + '-' + meta['mnc']
     }
   def reboot(self):
@@ -43,7 +44,7 @@ class CubeController(ControllerBase):
       reboot_request = requests.post('http://192.168.12.1/TMI/v1/gateway/reset?set=reboot', headers={'Authorization': 'Bearer ' + self.app_token})
     except:
       logging.critical("Could not post reboot request, exiting.")
-      sys.exit(2)
+      sys.exit(ExitStatus.API_ERROR.value)
     reboot_request.raise_for_status()
   # functions using authenticated web API endpoints
   def login_web(self):
@@ -56,7 +57,7 @@ class CubeController(ControllerBase):
       signal_request = requests.get('http://192.168.12.1/TMI/v1/gateway?get=all')
     except:
       logging.critical("Could not query signal status, exiting.")
-      sys.exit(2)
+      sys.exit(ExitStatus.API_ERROR.value)
     signal_request.raise_for_status()
     self.info_web = signal_request.json()
     return self.info_web
@@ -65,7 +66,10 @@ class CubeController(ControllerBase):
   def get_signal_info(self):
     info = self.get_all_info_web()
     lte_info = info['signal']['4g']['bands']
-    nr_info = info['signal']['5g']['bands']
+    if '5g' in info['signal']:
+      nr_info = info['signal']['5g']['bands']
+    else:
+      nr_info = []
 
     return {
       '4G': None if len(lte_info) == 0 else lte_info[0].upper(),
--- tmo_monitor/gateway/nokia.py
+++ tmo_monitor/gateway/nokia.py
@@ -5,6 +5,7 @@ import logging
 import requests
 import secrets
 import sys
+from ..status import ExitStatus
 
 class TrashCanController(ControllerBase):
   def __init__(self, username, password):
@@ -14,6 +15,7 @@ class TrashCanController(ControllerBase):
     self.csrf_token = None
     self.app_jar = None
     self.web_jar = None
+    self.device_info = None
 
   # functions using authenticated app API endpoints
   def login_app(self):
@@ -21,7 +23,7 @@ class TrashCanController(ControllerBase):
       login_request = requests.post('http://192.168.12.1/login_app.cgi', data={'name': self.username, 'pswd': self.password})
     except:
       logging.critical("Could not post login request, exiting.")
-      sys.exit(2)
+      sys.exit(ExitStatus.API_ERROR.value)
     login_request.raise_for_status()
 
     self.app_jar = requests.cookies.RequestsCookieJar()
@@ -35,7 +37,7 @@ class TrashCanController(ControllerBase):
       stat_request = requests.get('http://192.168.12.1/cell_status_app.cgi', cookies=self.app_jar)
     except:
       logging.critical("Could not query site info, exiting.")
-      sys.exit(2)
+      sys.exit(ExitStatus.API_ERROR.value)
 
     stat_request.raise_for_status()
     meta = stat_request.json()['cell_stat_lte'][0]
@@ -51,13 +53,23 @@ class TrashCanController(ControllerBase):
       nonce_request = requests.get('http://192.168.12.1/login_web_app.cgi?nonce')
     except:
       logging.critical("Could not query nonce, exiting.")
-      sys.exit(2)
+      sys.exit(ExitStatus.API_ERROR.value)
 
     nonce_request.raise_for_status()
     nonce_response = nonce_request.json()
     self.nonce = nonce_response['nonce']
 
-    user_pass_hash = self.sha256(self.username, self.password)
+    if self.get_firmware_version() < '1.2103.00.0338':
+      pass_hash_input = self.password
+    else:
+      if nonce_response['iterations'] >= 1:
+        raise Exception('Password strategy not implemented')
+      else:
+        r = self.password
+
+      pass_hash_input = r.lower()
+
+    user_pass_hash = self.sha256(self.username, pass_hash_input)
     user_pass_nonce_hash = self.sha256url(user_pass_hash, self.nonce)
     login_request_body = {
       'userhash': self.sha256url(self.username, self.nonce),
@@ -72,11 +84,12 @@ class TrashCanController(ControllerBase):
       login_request = requests.post('http://192.168.12.1/login_web_app.cgi', data=login_request_body)
     except:
       logging.critical("Could not post login request, exiting.")
-      sys.exit(2)
+      sys.exit(ExitStatus.API_ERROR.value)
     login_request.raise_for_status()
     self.web_jar = requests.cookies.RequestsCookieJar()
     self.web_jar.set('sid', login_request.cookies['sid'], domain='192.168.12.1', path='/')
-    self.web_jar.set('lsid', login_request.cookies['lsid'], domain='192.168.12.1', path='/')
+    if 'lsid' in login_request.cookies:
+      self.web_jar.set('lsid', login_request.cookies['lsid'], domain='192.168.12.1', path='/')
     login_response = login_request.json()
     self.csrf_token = login_response['token']
 
@@ -87,25 +100,43 @@ class TrashCanController(ControllerBase):
       reboot_request = requests.post('http://192.168.12.1/reboot_web_app.cgi', data={'csrf_token': self.csrf_token}, cookies=self.web_jar)
     except:
       logging.critical("Could not post reboot request, exiting.")
-      sys.exit(2)
+      sys.exit(ExitStatus.API_ERROR.value)
     reboot_request.raise_for_status()
 
   # functions using unauthenticated API endpoints
+  def get_device_info(self):
+    try:
+      device_info_req = requests.get('http://192.168.12.1/dashboard_device_info_status_web_app.cgi')
+    except:
+      logging.critical("Could not query device info, exiting.")
+      sys.exit(ExitStatus.API_ERROR.value)
+    device_info_req.raise_for_status()
+    return device_info_req.json()['device_app_status'][0]
+
+  def get_firmware_version(self):
+    try:
+      if not self.device_info:
+        self.device_info = self.get_device_info()
+    except:
+      logging.critical("Could not query firmware version, exiting.")
+      sys.exit(ExitStatus.API_ERROR.value)
+    return self.device_info['SoftwareVersion']
+
   def get_uptime(self):
     try:
-      uptime_req = requests.get('http://192.168.12.1/dashboard_device_info_status_web_app.cgi')
+      if not self.device_info:
+        self.device_info = self.get_device_info()
     except:
       logging.critical("Could not query modem uptime, exiting.")
-      sys.exit(2)
-    uptime_req.raise_for_status()
-    return uptime_req.json()['device_app_status'][0]['UpTime']
+      sys.exit(ExitStatus.API_ERROR.value)
+    return self.device_info['UpTime']
 
   def get_signal_info(self):
     try:
       signal_request = requests.get('http://192.168.12.1/fastmile_radio_status_web_app.cgi')
     except:
       logging.critical("Could not query signal status, exiting.")
-      sys.exit(2)
+      sys.exit(ExitStatus.API_ERROR.value)
     signal_request.raise_for_status()
     info = signal_request.json()
 
